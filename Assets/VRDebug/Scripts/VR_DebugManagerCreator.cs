5bc4472fe59cc57417673fc1382adfc044eb2683
Platinio <platinio94@gmail.com>
2019-12-27 20:04:57 -0300

    add VR_DebugManagerCreator to instante VR_Manager on game load

---
+++ Assets/VRDebug/Scripts/VR_DebugManagerCreator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace VRDebug
+{
+    public class VR_DebugManagerCreator 
+    {
+        [RuntimeInitializeOnLoadMethod]
+        private static void InitializeOnLoad()
+        {
+            GameObject go = Resources.Load("VR_DebugManager") as GameObject;
+            GameObject.Instantiate(go);
+        }
+    }
+}
+
